9b504b3a1e05deecf75fee78f9fe8e4ac05c7287
Joe Dimartino <j.dimartino11@gmail.com>
2020-12-01 15:52:36 -0500

    Remove store initialization
    Store will no longer be intialized before mounting the app to the DOM.
    With the addition of dynamic url routing... the App will now wait to
    read the url parameters before deciding which product to populate the
    store with.  Instead a blank store will be created and wrapped around
    the app in this phase.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import AppRouter from "./AppRouter";
 import { Provider } from 'react-redux';
-import { initializeStore, store } from './store/store.js';
+import store from './store/store.js';
 
 
 //call intializeStore from store file before mounting app this will populate the store with an initial api call before rendering the page
-initializeStore(1, () => {
-  var mountNode = document.getElementById("app");
-  ReactDOM.render(
+var mountNode = document.getElementById("app");
+ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <AppRouter/>
   </Provider>,
-  mountNode);
-})
+mountNode);
