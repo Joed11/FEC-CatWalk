f666ede8ed1737618be4fb8ac450edfe4dd70db4
Joe Dimartino <j.dimartino11@gmail.com>
2020-12-01 15:30:17 -0500

    Rework server to handle dynamic urls
    Server will now serve up same file for all routes other than reverse
    backend api routes ('/catwalk')
    
    ApiRoutes have been separated into their own router in the server folder
    under the name apiRouter.js

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const APP = express();
+const apiRouter = require('./server/apiRouter');
+const bodyParser = require('body-parser');
+const path = require('path');
+const compression = require('compression');
+const URL = require('url');
+
+const PORT = process.env.PORT || 3005;
+APP.use(compression());
+APP.use(bodyParser.json());
+APP.use(bodyParser.urlencoded({ extended: true }));
+APP.use(express.static(path.join(__dirname, 'dist')));
+
+APP.use('/catwalk', apiRouter);
+
+APP.use((req,res)=>{res.sendFile(`${__dirname}/dist/index.html`)});
+
+APP.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
